5dcf35c83ac1b5cd72a2b8e070a5aa4db19432be
cylexstair <74960918+cylexstair@users.noreply.github.com>
2023-09-15 12:23:18 +0200

    Update FizzBuzz.py

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,14 +1,9 @@
-def fizzBuzz(n):
-    if n % 3 == 0 and n % 5 == 0:
+def fizzBuzz((int)nombre):
+    if nombre % 3 == 0 and nombre % 5 == 0:
         return "FizzBuzz"
-    elif n % 3 == 0:
+    elif nombre % 3 == 0:
         return "Fizz"
-    elif n % 5 == 0:
+    elif nombre % 5 == 0:
         return "Buzz"
     else:
-        return str(n)
-
-#Demande à l'utilisateur d'entrer un nombre
-num = int(input("Entrez un nombre entier : "))
-result = fizzBuzz(num)
-print("Résultat de FizzBuzz pour", num, ":", result)
+        return str(nombre)
